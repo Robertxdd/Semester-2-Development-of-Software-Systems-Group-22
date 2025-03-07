f9f998e5f2feaba766bf11d21bf27a39dabc71ed
Robert <robert820404@gmail.com>
2025-03-07 21:19:06 +0100

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Semester2ProjectGroup22;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
