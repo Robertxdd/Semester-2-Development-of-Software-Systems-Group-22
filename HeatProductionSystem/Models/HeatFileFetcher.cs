d269f5de5883b4aabf7a218b008a87b398a375e9
Robert <robert820404@gmail.com>
2025-03-31 21:58:59 +0200

    modified the data types so it works properly

--- HeatProductionSystem/Models/HeatFileFetcher.cs
+++ HeatProductionSystem/Models/HeatFileFetcher.cs
@@ -1,13 +1,12 @@
 using System;
-using System.Collections.ObjectModel;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 
 namespace HeatProductionSystem;
 
 public class HeatFetcher
 {
-    public static List<HeatData> FetchHeatData() // fetching from the file and storing them in heatdata file test
+    public static List<HeatData> FetchHeatData() // fetching from the file and storing them in heatdata list
     {
         string heatFilePath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "HeatProductionSystem", "Assets", "2025 Heat Production Optimization - Danfoss Deliveries - Source Data Manager(SDM).csv");
 
@@ -15,37 +14,32 @@ public class HeatFetcher
 
         using (var reader = new StreamReader(heatFilePath))
         {
-
-            reader.ReadLine(); // read one line to skip header
-            reader.ReadLine();
-            reader.ReadLine();
+            reader.ReadLine(); // for skipping the header
+            reader.ReadLine(); 
+            reader.ReadLine(); 
 
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
                 var values = line.Split(',');
 
-            
+
                 var heatData = new HeatData
                 {
                     TimeFromW = values[0],
                     TimeToW = values[1],
-                    HeatDemandW = values[2],
-                    ElPriceW = values[3],
+                    HeatDemandW = double.TryParse(values[2], out var heatDemandW) ? heatDemandW : 0,
+                    ElPriceW = double.TryParse(values[3], out var elPriceW) ? elPriceW : 0,
                     TimeFromS = values[4],
                     TimeToS = values[5],
-                    HeatDemandS = values[6],
-                    ElPriceS = values[7]
+                    HeatDemandS = double.TryParse(values[6], out var heatDemandS) ? heatDemandS : 0,
+                    ElPriceS = double.TryParse(values[7], out var elPriceS) ? elPriceS : 0
                 };
 
                 heatDataList.Add(heatData);
-               
             }
-           
         }
 
-       
-        return heatDataList; 
+        return heatDataList;
     }
 }
-// fetching from the file and storing them in heatdata file test
