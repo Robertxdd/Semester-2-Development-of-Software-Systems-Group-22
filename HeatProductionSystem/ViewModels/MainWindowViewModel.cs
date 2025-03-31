9a559aa44eca93cd4dec66107c29beee9956dc80
Benjamin Markus Fellner <bfell24@student.sdu.dk>
2025-03-31 14:49:05 +0200

    minor changes to views + Productionunits view

--- HeatProductionSystem/ViewModels/MainWindowViewModel.cs
+++ HeatProductionSystem/ViewModels/MainWindowViewModel.cs
@@ -13,12 +13,15 @@ public partial class MainWindowViewModel : ViewModelBase
     private ViewModelBase DashboardView;
     private ViewModelBase optimizerView;
     private ViewModelBase SettingsView;
+    private ViewModelBase SimulationsView;
     public MainWindowViewModel()
     {
         dataView = new DataViewViewModel();
         DashboardView = new GridAssetViewViewModel();
         optimizerView = new OptimizerViewViewModel();
         SettingsView = new SettingsViewViewModel();
+        
+        SimulationsView = new SimulationsViewViewModel();
 
         selectedView = dataView; 
     }
@@ -26,29 +29,29 @@ public partial class MainWindowViewModel : ViewModelBase
     [RelayCommand]
     public void ChangeView(string viewID)
     {
-        Console.WriteLine("CLicked");
-        //Convert the input parameter to an int
         int.TryParse(viewID, out int _viewID);
         
         switch (_viewID)
         {
             case 0:
-                SelectedView = dataView;
+                SelectedView = DashboardView;
                 break;
             case 1:
-                SelectedView = DashboardView;
+                SelectedView = SettingsView;
                 break;
             case 2:
                 SelectedView = optimizerView;
                 break;
             case 3:
-                SelectedView = SettingsView;
-                break;
-            case 4:
                 SelectedView = dataView;
                 break;
+            case 4:
+                SelectedView = SimulationsView;
+                break;
             case 5:
-                SelectedView = DashboardView;
+                //Maybe to be implemented later: 
+                //A function that properly shuts down the program
+                Environment.Exit(0);
                 break;
             default:
                 break;
---
+++ HeatProductionSystem/Views/SimulationsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HeatProductionSystem.Views;
+
+public partial class SimulationsView : ContentControl
+{
+    public SimulationsView()
+    {
+        InitializeComponent();
+    }
+}
