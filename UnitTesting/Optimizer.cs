968411f8a48e060051e49374de046fd5202da8d3
NachoPalomero <ipalomerog@gmail.com>
2025-04-03 11:05:40 +0200

    Update Optimizer.cs

--- UnitTesting/Optimizer.cs
+++ UnitTesting/Optimizer.cs
@@ -1,19 +1,1 @@
-using System;
-using HeatProductionSystem.Models;
 
-namespace HeatProductionSystem;
-
-public class Optimizer
-{
-    private HeatFetcher heatFetcher;
-    private ProductionUnitsData productionUnitsData;
-
-    HeatFetcher heat_Fetcher = new HeatFetcher();
-    ProductionUnitsData production_Units_Data = new ProductionUnitsData();
-
-    public void ReadInformation()
-    {
-        var _heatData = HeatFetcher.FetchHeatData();
-        var _productionUnitsData = ProductionUnitsData.ProductionUnitsCollection();
-    }
-}
